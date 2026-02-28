0290abd8c6de9dc21c2594ae00f1e7f38df12225
Sherhan <Sherhanabid123@gmail.com>
2026-02-28 15:38:47 +0530

    style: remove hover plus icon overlay from gallery images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -28,9 +28,6 @@ const Gallery = () => {
                             style={{ transitionDelay: `${(index % 3) * 0.1}s` }}
                         >
                             <img src={src} alt={`Fridah Gallery Image ${index + 1}`} />
-                            <div className="gallery-overlay">
-                                <span className="plus-icon">+</span>
-                            </div>
                         </div>
                     ))}
                 </div>
