e496d08c8d2b4e847f3916efbc1929024ef514e5
Sherhan-a <Sherhanabid123@gmail.com>
2026-03-23 00:00:59 +0530

    fix: restore syntax and resolve eslint errors blocking vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,8 @@ function App() {
       smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
+    });
+
     // eslint-disable-next-line react-hooks/set-state-in-effect
     setLenisInstance(lenis);
 
