bb52f0421478a4d8dbfc5f32f0a38716d2e921cd
Sherhan-a <Sherhanabid123@gmail.com>
2026-03-25 23:51:01 +0530

    refactor: remove custom cursor to restore native OS interaction

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import Reservation from './components/Reservation';
 import CTASection from './components/CTASection';
 import Footer from './components/Footer';
 import Preloader from './components/Preloader';
-import CustomCursor from './components/CustomCursor';
 import Lenis from 'lenis';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -127,7 +126,7 @@ function App() {
     <>
       <Preloader onLoadingComplete={handleLoadingComplete} />
       <div className="film-grain"></div>
-      <CustomCursor />
+      <div className="film-grain"></div>
       
       <div className={`app-root ${isAppLoaded ? 'app-ready' : ''}`}>
         <div className="scroll-progress-container">
